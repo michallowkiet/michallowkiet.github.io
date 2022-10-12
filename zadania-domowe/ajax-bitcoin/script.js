683821de9e9655e767e0d0d3e5aa24947371494a
Michał Łowkiet <mlowkiet@gmail.com>
2022-10-12 14:16:13 +0200

    ajax bitcoin

---
+++ zadania-domowe/ajax-bitcoin/script.js
@@ -0,0 +1,58 @@
+const line = '<i class="fas fa-minus"></i>';
+const arrowUp = '<i class="fas fa-arrow-up"></i>';
+const arrowDown = '<i class="fas fa-arrow-down"></i>';
+
+const buyP = document.querySelector(".buy");
+const sellP = document.querySelector(".sell");
+
+// $(function () {
+//   $.getJSON(
+//     "https://api.blockchain.com/v3/exchange/tickers/BTC-USD",
+//     function (data) {
+//       console.log(data);
+//     }
+//   );
+// });
+
+$(function () {
+  function arrowDirection(data, el) {
+    if (data === Number(el.textContent))
+      el.innerHTML = `${data.USD.buy} ${line}`;
+    if (data > Number(el.textContent))
+      el.innerHTML = `${data.USD.buy} ${arrowUp}`;
+    if (data < Number(el.textContent))
+      el.innerHTML = `${data.USD.buy} ${arrowDown}`;
+  }
+
+  function getData() {
+    $.getJSON(
+      "https://api.blockchain.com/v3/exchange/tickers/BTC-USD",
+      function (data) {
+        if (buyP.textContent === "" && sellP.textContent === "") {
+          buyP.innerHTML = `${data.last_trade_price} ${line}`;
+          sellP.innerHTML = `${data.price_24h} ${line}`;
+        }
+
+        if (data.last_trade_price === Number(buyP.textContent))
+          buyP.innerHTML = `${data.last_trade_price} ${line}`;
+        if (data.last_trade_price > Number(buyP.textContent))
+          buyP.innerHTML = `${data.last_trade_price} ${arrowUp}`;
+        if (data.last_trade_price < Number(buyP.textContent))
+          buyP.innerHTML = `${data.last_trade_price} ${arrowDown}`;
+
+        if (data.price_24h === Number(sellP.textContent))
+          sellP.innerHTML = `${data.price_24h} ${line}`;
+        if (data.price_24h > Number(sellP.textContent))
+          sellP.innerHTML = `${data.price_24h} ${arrowUp}`;
+        if (data.price_24h < Number(sellP.textContent))
+          sellP.innerHTML = `${data.price_24h} ${arrowDown}`;
+      }
+    );
+  }
+
+  getData();
+
+  $("button").on("click", function () {
+    getData();
+  });
+});
